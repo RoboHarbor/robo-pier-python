f8df837f0f6cf8856f04d1cbd9d090d11a1fcfca
Thoren Lederer <lederer@adrodev.de>
2024-05-30 23:57:31 +0200

    Renamed the docker container

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -7,6 +7,6 @@ docker build -t roboharbor/validate-robot .
 docker push roboharbor/validate-robot
 
 
-docker build --no-cache -t roboharbor/robo-pier-python-container .
+docker build --no-cache -t roboharbor/robo-pier-python .
 
-docker push roboharbor/robo-pier-python-container
+docker push roboharbor/robo-pier-python
