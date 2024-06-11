f9b747f389b3da9144b5eba4170a39f6429cbcd0
Thoren Lederer <lederer@adrodev.de>
2024-06-11 21:56:28 +0200

    changed the running of the python version

--- entry_point.py
+++ entry_point.py
@@ -20,18 +20,20 @@ class PythonRobot(ProcessCallback):
     def stream_command(
             self,
             args,
-            *,
             stdout_handler=logging.info,
             stderr_handler=logging.error,
             check=True,
             text=True,
             stdout=PIPE,
             stderr=PIPE,
+            cwd=None,
             **kwargs,
     ):
         """Mimic subprocess.run, while processing the command output in real time."""
-        with Popen(args, text=text, stdout=stdout, stderr=stderr, **kwargs) as process:
+        with Popen(args, text=text, stdout=stdout, stderr=stderr, cwd=cwd, **kwargs) as process:
+            print("Process started: ", process.args)
             with ThreadPoolExecutor(2) as pool:  # two threads to handle the streams
+                print("Process started 2: ", process.args)
                 exhaust = partial(pool.submit, partial(deque, maxlen=0))
                 exhaust(stdout_handler(line[:-1]) for line in process.stdout)
                 exhaust(stderr_handler(line[:-1]) for line in process.stderr)
@@ -50,23 +52,20 @@ class PythonRobot(ProcessCallback):
 
         try:
             print("Installing requirements")
-            subprocess.run("pyenv local "+python_version+" && pip install -r requirements.txt ", cwd=full_app_path,
-                                        shell=True, check=True)
+            subprocess.run("pyenv local "+python_version+" && pip install -r requirements.txt ",
+                                        cwd=full_app_path,
+                                        shell=True)
 
         except Exception as e:
             print("Error: ", str(e))
 
         script = self.get_config_value('script')
-        """process = subprocess.Popen("python"+python_version+" "+script,
-                                   shell=True,
-                                   cwd=full_app_path,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)"""
-
+        print("Running script: ", script)
         rc = self.stream_command(
             ["python"+python_version, script],
             cwd=full_app_path,
-             stdout_handler = print, stderr_handler = print
+            stdout_handler = print,
+            stderr_handler = print
         )
 
         if rc.returncode != 0:
